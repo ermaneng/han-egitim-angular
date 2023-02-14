60150bc18b838b3cfee5a20a3640cf8bcd30a63e
ERMAN ENGINLER <ermaneng@gmail.com>
2023-02-14 19:01:58 +0300

    interpolators, 2 way bindings, interface

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'web';
+  title:string = 'web';
+
+
+  /*
+  myObj:PersonInterface = {
+    name: "John",
+    age: 30
+  }*/
+
+  myObj:Partial<PersonInterface> = {
+    name: "John",
+    age: 30
+  }
+
+  buttonClicked(event:Event):void{
+    console.info(event)
+    alert(this.title);
+    this.title = '123';
+  }
+}
+
+export interface PersonInterface{
+  name: string,
+  surname: string,
+  age: number,
+  active: boolean
 }
